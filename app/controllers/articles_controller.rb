5bc16b43bfaf7270e828afe0463ce79b610dd8e3
syahmeds <10323804+syahmeds@users.noreply.github.com>
2019-07-27 12:55:40 -0700

    Added add new article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,27 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+    # @article = Article.new(article_params)
+    # @article.save
     @article = Article.new(article_params)
-    @article.save
-    redirect_to_articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   def show
-
+    @article = Article.find(params[:id])
   end
 
   private
