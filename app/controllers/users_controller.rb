1aa0ae27c1e3302e58e338419f7aa5d81438ece4
syahmeds <10323804+syahmeds@users.noreply.github.com>
2019-07-30 09:21:53 -0700

    Updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show]
 
+  before_action :require_same_user, only: [:edit, :update]
+
   def index
 
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -72,4 +74,17 @@ class UsersController < ApplicationController
 
   end
 
+  def require_same_user
+
+    if current_user != @user
+
+      flash[:danger] = "You can only edit your own account"
+
+      redirect_to root_path
+
+    end
+
+  end
+
 end
+
